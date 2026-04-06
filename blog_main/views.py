8458a04b4644e08aa7a4725c66bc199a26874fac
Anjas2005 <anjasvaidya2020@gmail.com>
2026-04-06 13:40:53 +0530

    registration and login functionality

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-
+from django.contrib import auth
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
 from blogs.models import Blog, Category
 from assignments.models import About
+from .forms import RegistrationForm
 
 
 def home(request):
@@ -22,3 +24,42 @@ def home(request):
         "about": about,
     }
     return render(request, "home.html", context)
+
+
+#  email:test@gmail.com user: test@123 pass=Pass@123
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("register")
+        else:
+            print(form.errors)
+    else:
+        form = RegistrationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "register.html", context)
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect("home")
+    form = AuthenticationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "login.html", context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
