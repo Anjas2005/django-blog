5d38d12966002e7c5aa3dc7f70501b8b540b81fb
Anjas2005 <anjasvaidya2020@gmail.com>
2026-04-20 17:36:41 +0530

    comment feature added

--- blogs/models.py
+++ blogs/models.py
@@ -37,3 +37,12 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.comment
--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.db.models import Q
 from blog_main.views import home
 
-from .models import Blog, Category
+from .models import Blog, Category, Comment
 
 # Create your views here.
 
@@ -24,9 +24,22 @@ def posts_by_category(request, category_id):
 
 
 def blogs(request, slug):
+    # Comments
     single_blog = get_object_or_404(Blog, slug=slug, status="Published")
+    if request.method == "POST":
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST["comment"]
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+    comments = Comment.objects.filter(blog=single_blog)
+    comment_count = comments.count()
+    print("Comments=>", comments)
     context = {
         "single_blog": single_blog,
+        "comments": comments,
+        "comment_count": comment_count,
     }
     return render(request, "blogs.html", context)
 
